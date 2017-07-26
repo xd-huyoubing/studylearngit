69e95071d5e72515365d7da7fc6104647db4ce16
xd-yuyoubing <18291424496@163.com>
2017-07-26 10:00:04 +0800

    第一次修改文件

--- src/main/java/xidian/edu/studylearngit/StudyLearnGit.java
+++ src/main/java/xidian/edu/studylearngit/StudyLearnGit.java
@@ -4,4 +4,8 @@ public class StudyLearnGit {
     public static void main(String[] args) {
         System.out.println("第一次学习idea上传git项目");
     }
+
+    public void version(){
+        String version = "1.0";
+    }
 }
