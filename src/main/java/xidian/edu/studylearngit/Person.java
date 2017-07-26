647a3e8814df73d18be1b1c80ffda7aa7ff6f1d7
xd-yuyoubing <18291424496@163.com>
2017-07-26 10:33:24 +0800

    第一次修改文件dddd     FDF

---
+++ src/main/java/xidian/edu/studylearngit/Person.java
@@ -0,0 +1,9 @@
+package xidian.edu.studylearngit;
+
+public class Person {
+    private int age;
+    private String name;
+    private String sex;
+
+
+}
--- src/main/java/xidian/edu/studylearngit/StudyLearnGit.java
+++ src/main/java/xidian/edu/studylearngit/StudyLearnGit.java
@@ -9,4 +9,6 @@ public class StudyLearnGit {
     public void version() {
         String version = "1.0";
     }
+
+
 }
