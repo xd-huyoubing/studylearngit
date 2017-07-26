d94f4898ecc3a1578fc14e30eb0cd32744eec018
xd-yuyoubing <18291424496@163.com>
2017-07-26 10:13:48 +0800

    第一次修改文件dddd

--- src/main/java/xidian/edu/studylearngit/StudyLearnGit.java
+++ src/main/java/xidian/edu/studylearngit/StudyLearnGit.java
@@ -1,11 +1,12 @@
 package xidian.edu.studylearngit;
 
 public class StudyLearnGit {
+    private static String age = "3242";
     public static void main(String[] args) {
         System.out.println("第一次学习idea上传git项目");
     }
 
-    public void version(){
+    public void version() {
         String version = "1.0";
     }
 }
